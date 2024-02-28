13e838dbb01c920dc3aaed2ee886008f8ce8fefa
jon-jc <jonathancho.jc@gmail.com>
2024-02-27 16:07:38 -0800

    Added dropdown selection, has search functionality. Added the functionlity to make new stores through the store switcher.

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -28,7 +29,9 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>NavBar</div>
+      <div>
+        <Navbar />
+      </div>
       {children}
     </>
   );
